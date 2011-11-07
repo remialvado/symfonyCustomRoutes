85306b5e6bee8c854d647a962c020067c410437c
ralvado <ralvado@bestofmedia.com>
2011-11-07 16:27:24 +0100

    clean up unnecessary stuffs

--- apps/frontend/modules/demo/templates/indexSuccess.php
+++
@@ -1,4 +0,0 @@
-<ul>
-  <li><a href="/signedUrl?lang=fr&signature_value=e6bea0e485d16bbbac937987b9dda57f83e7ec99">Valid Signed Url</a></li>
-  <li><a href="/signedUrl">Invalid Signed Url (check HTTP Header)</a></li>
-</ul>
